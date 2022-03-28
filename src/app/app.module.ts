b18c4f0006fe82c6b8e18a984365f0f1e7fd2e20
E22T07 <estermolina323@gmail.com>
2022-03-28 09:22:19 -0600

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoPrimeNgTableComponent } from './components/demo-prime-ng-table/demo-prime-ng-table.component';
-import {TableModule} from "primeng/table";
-import {TableExampleComponent} from "./Views/table-example/table-example.component";
+import { TableModule } from 'primeng/table';
+import { TableExampleComponent } from './Views/table-example/table-example.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoPrimeNgTableComponent,
-    TableExampleComponent
+    TableExampleComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    TableModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, TableModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
